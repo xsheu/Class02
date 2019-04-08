4cabc7c0938b588fb554e5b89017be73503af83b
Guang Yih Sheu <xsheu@hotmail.com>
2019-04-08 20:20:44 +0800

    RBF network (Test version)

---
+++ Class02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Class02
+//
+//  Created by 許光毅 on 2019/4/8.
+//  Copyright © 2019 許光毅. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
